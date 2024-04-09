39ed0c69651170ecf4505587ab87cba8242b9d30
Palak Ramani <155879317+palakxcode@users.noreply.github.com>
2024-04-09 18:03:37 +0530

    Add files via upload

---
+++ login_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  final _formkey = GlobalKey<FormState>();
+
+  Future<bool> loginUser(BuildContext context) async {
+    if (_formkey.currentState != null && _formkey.currentState!.validate()) {
+      print(userNameController.text);
+      print(passwordController.text);
+
+      // Perform login logic here
+      // Return true if login is successful, false otherwise
+      return true;
+    } else {
+      print('not successful!');
+      return false;
+    }
+  }
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+
+              //TODO: Add Username & Password text fields
+
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value != null &&
+                            value.isNotEmpty &&
+                            !value.endsWith('@vitstudent.ac.in')) {
+                          return "You  should enter your vit email ";
+                        } else if (value != null && value.isEmpty) {
+                          return "Please type your email";
+                        }
+                        return null;
+                      },
+                      controller: userNameController,
+                      decoration: InputDecoration(
+                          hintText: 'Add your email',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: 'Type your password',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder()),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final isLoginSuccessful = await loginUser(context);
+                  if (isLoginSuccessful) {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => VNewsHomePage()));
+                  }
+                },
+                child: Text('SIGN IN'),
+              ),
+              GestureDetector(
+                onTap: () {
+                  //todo: Navigate to browser
+                  print('Link clicked!');
+                },
+                child: Column(
+                  children: [
+                    Text('Find us on'),
+                    Text('https://vtopcc.com'),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
