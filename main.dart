b1786ce9d6da9f3a5f6a915773ff65ae98969cfe
Palak Ramani <155879317+palakxcode@users.noreply.github.com>
2024-04-10 14:13:28 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -30,7 +30,6 @@ class VNewsHomePage extends StatefulWidget {
 
 class _VNewsHomePageState extends State<VNewsHomePage> {
   int _selectedIndex = 0;
-
   final List<Widget> _pages = [
     PrimePage(),
     EventsPage(),
@@ -49,17 +48,54 @@ class _VNewsHomePageState extends State<VNewsHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('V-NEWS')),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => RegistrationFormPage()));
-            },
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: Material(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20.0), // Rounded corners at the bottom
           ),
-        ],
+          elevation: 4.0, // Mild shadow
+          child: AppBar(
+            title: Center(
+              // Center the title
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 18.0, vertical: 8.0, // Padding around the text
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.amber[800], // Background color of the container
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25.0), // Rounded corners
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5), // Shadow color
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Text('V-NEWS', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            backgroundColor: Colors.white,
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/my_logo.png'),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.account_circle),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => RegistrationFormPage(),
+                  ));
+                },
+              ),
+            ],
+          ),
+        ),
       ),
       body: IndexedStack(
         index: _selectedIndex,
