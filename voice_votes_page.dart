6d8967be0ea442629bf5f7426e34e7fb94503381
Palak Ramani <155879317+palakxcode@users.noreply.github.com>
2024-04-10 08:44:34 +0530

    Update voice_votes_page.dart

--- voice_votes_page.dart
+++ voice_votes_page.dart
@@ -7,7 +7,12 @@ class VoicesVotesContent extends StatefulWidget {
 }
 
 class _VoicesVotesContentState extends State<VoicesVotesContent> {
-  List<Poll> polls = [];
+  List<Poll> polls = [
+    Poll(question: 'Do you like Flutter?', options: ['Yes', 'No', 'Maybe']),
+    Poll(
+        question: 'Your favorite programming language?',
+        options: ['Dart', 'Python', 'JavaScript']),
+  ];
 
   void _addPoll() {
     // Function to show dialog and add a poll
@@ -18,56 +23,68 @@ class _VoicesVotesContentState extends State<VoicesVotesContent> {
         String question = '';
         List<String> options = ['', ''];
 
-        return AlertDialog(
-          title: Text('Create a New Poll'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(hintText: 'Enter poll question'),
-                  onChanged: (value) {
-                    question = value;
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: Text('Create a New Poll'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    TextField(
+                      decoration:
+                          InputDecoration(hintText: 'Enter poll question'),
+                      onChanged: (value) {
+                        question = value;
+                      },
+                    ),
+                    ...options.map((option) {
+                      return TextField(
+                        decoration:
+                            InputDecoration(hintText: 'Enter an option'),
+                        onChanged: (value) {
+                          options[options.indexOf(option)] = value;
+                        },
+                      );
+                    }).toList(),
+                    if (options.length < 4)
+                      ElevatedButton(
+                        child: Text('Add Option'),
+                        onPressed: () {
+                          setState(() {
+                            options.add('');
+                          });
+                        },
+                      ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
                   },
                 ),
-                ...options.map((option) {
-                  return TextField(
-                    decoration: InputDecoration(hintText: 'Enter an option'),
-                    onChanged: (value) {
-                      options[options.indexOf(option)] = value;
-                    },
-                  );
-                }).toList(),
-                ElevatedButton(
-                  child: Text('Add Option'),
+                TextButton(
+                  child: Text('Submit'),
                   onPressed: () {
-                    setState(() {
-                      options.add('');
-                    });
+                    if (question.isNotEmpty &&
+                        options.every((option) => option.isNotEmpty)) {
+                      setState(() {
+                        polls.add(Poll(question: question, options: options));
+                      });
+                      Navigator.of(context).pop();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Poll request sent to admin'),
+                        ),
+                      );
+                    }
                   },
                 ),
               ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Submit'),
-              onPressed: () {
-                if (question.isNotEmpty &&
-                    options.every((option) => option.isNotEmpty)) {
-                  setState(() {
-                    polls.add(Poll(question: question, options: options));
-                  });
-                  Navigator.of(context).pop();
-                }
-              },
-            ),
-          ],
+            );
+          },
         );
       },
     );
@@ -91,6 +108,59 @@ class _VoicesVotesContentState extends State<VoicesVotesContent> {
   }
 }
 
+class PollCard extends StatefulWidget {
+  final Poll poll;
+
+  PollCard({required this.poll});
+
+  @override
+  _PollCardState createState() => _PollCardState();
+}
+
+class _PollCardState extends State<PollCard> {
+  String? selectedOption;
+  bool isSubmitted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            title: Text(widget.poll.question),
+            subtitle: widget.poll.isOpen ? Text('Vote Now') : Text('Closed'),
+          ),
+          ...widget.poll.options.map((option) => RadioListTile<String>(
+                title: Text(option),
+                value: option,
+                groupValue: selectedOption,
+                onChanged: widget.poll.isOpen && !isSubmitted
+                    ? (String? value) {
+                        setState(() {
+                          selectedOption = value;
+                          // Here you would also handle saving the vote
+                        });
+                      }
+                    : null,
+              )),
+          if (widget.poll.isOpen && !isSubmitted)
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isSubmitted = true;
+                });
+                // Handle logic to close the poll and submit the votes
+              },
+              child: Text('Submit Vote'),
+            ),
+          if (isSubmitted) Text('Submitted'),
+        ],
+      ),
+    );
+  }
+}
+
 class Poll {
   String question;
   List<String> options;
@@ -100,53 +170,4 @@ class Poll {
   Poll({required this.question, required this.options})
       : votes = Map.fromIterable(options, key: (e) => e, value: (e) => 0),
         isOpen = true;
-}
-
-class PollCard extends StatelessWidget {
-  final Poll poll;
-
-  PollCard({required this.poll});
-
-  @override
-  Widget build(BuildContext context) {
-    // Determine the option with the maximum votes
-    String leadingOption = poll.votes.keys.isNotEmpty
-        ? poll.votes.keys.firstWhere(
-            (key) => poll.votes[key] == poll.votes.values.reduce(max),
-            orElse: () =>
-                poll.options.first, // Provide a default non-null String value
-          )
-        : poll.options.first; // Default value if no votes are present
-
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(poll.question),
-            subtitle: poll.isOpen ? Text('Vote Now') : Text('Closed'),
-          ),
-          ...poll.options.map((option) => ListTile(
-                title: Text(option),
-                leading: Radio(
-                  value: option,
-                  groupValue: leadingOption,
-                  onChanged: poll.isOpen
-                      ? (String? value) {
-                          // Handle vote logic
-                        }
-                      : null,
-                ),
-              )),
-          poll.isOpen
-              ? ElevatedButton(
-                  onPressed: () {
-                    // Handle close poll logic
-                  },
-                  child: Text('Close Poll'),
-                )
-              : Container(),
-        ],
-      ),
-    );
-  }
 }
